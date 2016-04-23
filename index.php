efe6db732d6e714b6a0b75740bb1d0bbf3976747
Prachi Manchanda <prachi121096@gmail.com>
2016-04-23 17:11:40 +0530

    improved look+added screengrab

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<strong>Quote of the Day</strong>
-
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+	<meta name="author" content="Prachi Manchanda">
+	<meta name="description" content="Quote of the Day">
+	<title>Quote of the Day</title>
+</head>
+<body bgcolor="#98AFC7">
+	<center>			
+		<h1><font face ="Tw Cen MT" color="#151b54">Quote of the Day</h1>
+		<h3><font face ="Tw Cen MT" color="#151b54">Powered By</h3>
+		<img src="images/logo.gif" alt="GoodReads" >
+	</center>
+	<br>
+</body>
+</html>
 <?php
 
 	function get_curl($url)
@@ -24,5 +40,5 @@
 
 		$res=str_replace("href=", "",$res);
 	
-	echo ($res);
+	echo $res;
 ?>
