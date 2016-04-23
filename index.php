016d9ef99db1e32f061155afa05c66c60fe99444
Prachi Manchanda <prachi121096@gmail.com>
2016-04-23 17:24:44 +0530

    code restructured

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+	require_once 'inc/function.inc.php';
+?>
 <!doctype html>
 <html>
 <head>
@@ -18,18 +21,6 @@
 </html>
 <?php
 
-	function get_curl($url)
-	{
-		$curl_handle=curl_init();
-		
-		curl_setopt($curl_handle, CURLOPT_URL, $url);
-		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 5);
-		$result= curl_exec($curl_handle);
-		return $result;
-	}
-		
-	
 		$res = get_curl("http://www.goodreads.com/quotes_of_the_day");
 		
 		$start = strpos($res, 'About this quote:');
